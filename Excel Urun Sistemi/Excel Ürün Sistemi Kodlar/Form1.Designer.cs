c9aef369bd1ce6e392d057e24eb8981a54faf0e2
Erkan Bostan <profesyonel59@gmail.com>
2022-03-10 05:09:18 +0300

    Excel Ürün Sistemi

---
+++ Excel Urun Sistemi/Excel Ürün Sistemi Kodlar/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Excel_Ürün_Sistemi_Kodlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton5 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton6 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit3 = new DevExpress.XtraEditors.TextEdit();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.Fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.Fbd2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Opf1 = new System.Windows.Forms.OpenFileDialog();
+            this.Opf2 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton1.ImageOptions.SvgImage")));
+            this.simpleButton1.Location = new System.Drawing.Point(12, 20);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(281, 51);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Hedef Dizin";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.simpleButton2.Appearance.Options.UseFont = true;
+            this.simpleButton2.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton2.ImageOptions.SvgImage")));
+            this.simpleButton2.Location = new System.Drawing.Point(12, 103);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(281, 54);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "Kopya Dizin";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.simpleButton3.Appearance.Options.UseFont = true;
+            this.simpleButton3.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton3.ImageOptions.SvgImage")));
+            this.simpleButton3.Location = new System.Drawing.Point(20, 163);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(232, 61);
+            this.simpleButton3.TabIndex = 2;
+            this.simpleButton3.Text = "Excel Dosya";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.simpleButton4.Appearance.Options.UseFont = true;
+            this.simpleButton4.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton4.ImageOptions.SvgImage")));
+            this.simpleButton4.Location = new System.Drawing.Point(765, 200);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(232, 61);
+            this.simpleButton4.TabIndex = 3;
+            this.simpleButton4.Text = "Excel \' e Çek";
+            this.simpleButton4.Click += new System.EventHandler(this.simpleButton4_Click);
+            // 
+            // simpleButton5
+            // 
+            this.simpleButton5.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.simpleButton5.Appearance.Options.UseFont = true;
+            this.simpleButton5.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton5.ImageOptions.SvgImage")));
+            this.simpleButton5.Location = new System.Drawing.Point(278, 200);
+            this.simpleButton5.Name = "simpleButton5";
+            this.simpleButton5.Size = new System.Drawing.Size(232, 60);
+            this.simpleButton5.TabIndex = 4;
+            this.simpleButton5.Text = "Sipariş Oluştur";
+            this.simpleButton5.Click += new System.EventHandler(this.simpleButton5_Click);
+            // 
+            // simpleButton6
+            // 
+            this.simpleButton6.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.simpleButton6.Appearance.Options.UseFont = true;
+            this.simpleButton6.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton6.ImageOptions.Image")));
+            this.simpleButton6.Location = new System.Drawing.Point(527, 200);
+            this.simpleButton6.Name = "simpleButton6";
+            this.simpleButton6.Size = new System.Drawing.Size(232, 60);
+            this.simpleButton6.TabIndex = 5;
+            this.simpleButton6.Text = "Destek";
+            this.simpleButton6.Click += new System.EventHandler(this.simpleButton6_Click);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(299, 25);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Size = new System.Drawing.Size(889, 46);
+            this.textEdit1.TabIndex = 6;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(299, 111);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.textEdit2.Properties.Appearance.Options.UseFont = true;
+            this.textEdit2.Size = new System.Drawing.Size(889, 46);
+            this.textEdit2.TabIndex = 7;
+            // 
+            // textEdit3
+            // 
+            this.textEdit3.Location = new System.Drawing.Point(20, 230);
+            this.textEdit3.Name = "textEdit3";
+            this.textEdit3.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.textEdit3.Properties.Appearance.Options.UseFont = true;
+            this.textEdit3.Size = new System.Drawing.Size(232, 28);
+            this.textEdit3.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(1002, 279);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 334);
+            this.listBox1.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.AliceBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 272);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(984, 335);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(1002, 268);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(240, 2);
+            this.panel1.TabIndex = 12;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(1002, 236);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(154, 24);
+            this.labelControl1.TabIndex = 11;
+            this.labelControl1.Text = "★ Eksik Resimler";
+            // 
+            // Opf1
+            // 
+            this.Opf1.FileName = "openFileDialog1";
+            // 
+            // Opf2
+            // 
+            this.Opf2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 619);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textEdit3);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.simpleButton6);
+            this.Controls.Add(this.simpleButton5);
+            this.Controls.Add(this.simpleButton4);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel Ürün Sistemi";
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private DevExpress.XtraEditors.SimpleButton simpleButton5;
+        private DevExpress.XtraEditors.SimpleButton simpleButton6;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.TextEdit textEdit3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.FolderBrowserDialog Fbd;
+        private System.Windows.Forms.FolderBrowserDialog Fbd2;
+        private System.Windows.Forms.OpenFileDialog Opf1;
+        private System.Windows.Forms.OpenFileDialog Opf2;
+    }
+}
+
---
+++ Excel Urun Sistemi/Excel Ürün Sistemi Kodlar/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using DevExpress.XtraEditors;
+using Microsoft.Office.Interop.Excel;
+
+namespace Excel_Ürün_Sistemi_Kodlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string nereye = "", neyinismi = "", bunu = "", excel = "";
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (Fbd.ShowDialog() == DialogResult.OK)
+            {
+
+                bunu = Fbd.SelectedPath.ToString();
+                textEdit1.Text = bunu;
+            }
+            else
+            {
+                XtraMessageBox.Show("Lütfen Kopyalanacak Dosya Yolunu Belirleyiniz...", "Uyarı Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            if (Fbd2.ShowDialog() == DialogResult.OK)
+            {
+                nereye = Fbd2.SelectedPath.ToString();
+                textEdit2.Text = nereye;
+            }
+            else
+            {
+                XtraMessageBox.Show("Dosyanın Kopyalanacağı Klasörü Seçmediniz...", "Uyarı Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        int kopyasayi;
+
+        private void simpleButton6_Click(object sender, EventArgs e)
+        {
+            XtraMessageBox.Show("0536 321 72 19 - Ulaşım Sağlayabilirsiniz...", "Bilgilendirme Mesajı", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+        }
+
+        private void simpleButton5_Click(object sender, EventArgs e)
+        {
+            if (!textEdit1.Text.Equals("") && !textEdit2.Text.Equals("") && !textEdit3.Text.Equals(""))
+            {
+                neyinismi = Convert.ToString(dataGridView1.Columns[0].HeaderText.ToString());
+
+                kopyasayi = Convert.ToInt32(dataGridView1.Columns[1].HeaderText.ToString());
+                if (File.Exists(bunu + "\\" + neyinismi + ".jpg") == true)
+                {
+                    if (File.Exists(nereye + "\\" + neyinismi + ".jpg") == false)
+                    {
+                        File.Copy(bunu + "\\" + neyinismi + ".jpg", nereye + "\\" + neyinismi + ".jpg");
+
+                    }
+
+                }
+                else
+                {
+                    listBox1.Items.Add(neyinismi + ".jpg");
+
+                }
+
+
+                for (int j = 0; j < dataGridView1.RowCount - 1; j++)
+                {
+                    if (j == 54)
+                    {
+                        kopyasayi = Convert.ToInt32(dataGridView1.Rows[j].Cells[1].Value.ToString());
+
+                    }
+                    else
+                    {
+                        kopyasayi = Convert.ToInt32(dataGridView1.Rows[j].Cells[1].Value.ToString());
+
+                    }
+
+                    for (int i = 1; i <= kopyasayi; i++)
+                    {
+                        neyinismi = dataGridView1.Rows[j].Cells[0].Value.ToString();
+                        if (File.Exists(bunu + "\\" + neyinismi + ".jpg") == true)
+                        {
+                            if (File.Exists(nereye + "\\" + neyinismi + "(" + i.ToString() + ")" + ".jpg") == false)
+                            {
+                                File.Copy(bunu + "\\" + neyinismi + ".jpg", nereye + "\\" + neyinismi + "(" + i.ToString() + ")" + ".jpg");
+
+                            }
+
+                        }
+                        else
+                        {
+                            listBox1.Items.Add(neyinismi + ".jpg");
+                            goto geçla;
+                        }
+
+
+
+                    }
+                geçla:;
+                }
+                XtraMessageBox.Show("Dosya Kopyalama İşlemi Başarılı", "Dosya Kopyalandı...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            else
+            {
+                XtraMessageBox.Show("Lütfen Dosya Yolları Kısımlarını Doldurunuz...", "Uyarı Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            Opf1.Filter = "Xlsx Dosyaları |*.xlsx| Bütün Dosyalar|*.*";
+            if (Opf1.ShowDialog() == DialogResult.OK)
+            {
+                excel = Opf1.FileName;
+                textEdit3.Text = excel;
+                simpleButton3_Click(sender, e);
+            }
+            else
+            {
+                XtraMessageBox.Show("Lütfen Excel Dosya Yolunu Belirleyiniz...", "Uyarı Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+            if (Opf2.ShowDialog() == DialogResult.OK)
+            {
+                if (listBox1.Items.Count > 0)
+                {
+
+
+                    int adet = listBox1.Items.Count;
+                    if (listBox1.Items.Count > 0)
+                    {
+                        Random rnd = new Random();
+                        int sayi = rnd.Next();
+                        string dosyayolu;
+                        dosyayolu = Opf2.FileName /*+ "\\" + "Olmayan Resimler" + sayi.ToString() + ".xlsx"*/;
+
+                        Microsoft.Office.Interop.Excel.Application excelapp = new Microsoft.Office.Interop.Excel.Application();
+                        excelapp.Visible = false;
+                        object missing = Type.Missing;
+
+                        Workbook calismakitabı = excelapp.Workbooks.Add(missing);
+                        calismakitabı = excelapp.Workbooks.Open(dosyayolu);
+
+                        Worksheet shet1 = (Worksheet)calismakitabı.Sheets[1];
+
+                        int satir = 1;
+                        int sütun = 1;
+                        for (int i = 0; i < adet; i++)
+                        {
+                            for (int j = 0; j < 1; j++)
+                            {
+                                Range myrange1 = (Range)shet1.Cells[satir + i, sütun + j];
+                                myrange1.Value2 = listBox1.Items[i] == null ? "" : listBox1.Items[i];
+                                myrange1.Select();
+                            }
+
+                        }
+
+
+                        object False = false;
+                        object True = true;
+                        XlFileFormat format = XlFileFormat.xlWorkbookDefault;
+                        calismakitabı.SaveAs(dosyayolu, format, missing, missing, missing, False, XlSaveAsAccessMode.xlNoChange, missing, false, missing, missing, missing);
+                        PrintDialog printdialog = new PrintDialog();
+                        printdialog.PrintToFile = true;
+                        calismakitabı.PrintOutEx(missing, missing, printdialog.PrinterSettings.Copies, false, printdialog.PrinterSettings.PrinterName, printdialog.PrinterSettings.PrintToFile, printdialog.PrinterSettings.Collate, printdialog.PrinterSettings.PrintFileName);
+                        excelapp.Quit();
+                    }
+                }
+                else
+                {
+                    XtraMessageBox.Show("Lütfen Aktarılacak Excel Dosyasını Seçiniz...", "Uyarı Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            else
+            {
+                XtraMessageBox.Show("Lütfen Aktarılacak Dosya İsimlerini Yazdırınız...", "Uyarı Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
